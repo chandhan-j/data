052ef2c0689d4fcb3e4f9ea7cdca5f6303c0e226
chandhan-j <64291457+chandhan-j@users.noreply.github.com>
2021-03-20 11:27:11 -0400

    Update data.py

--- data.py
+++ data.py
@@ -1,95 +1,77 @@
-def mean():
-    import csv
+from collections import Counter
+import csv
 
-    with open('SOCR-HeightWeight.csv', newline = '') as f:
-        reader = csv.reader(f)
-        file_data = list(reader)
+def mean(total_weight, total_entries):
+    mean = total_weight / total_entries
+    print(f"Mean (Average) is -> {mean:2f}")
 
-    file_data.pop(0)
-
-    new_data = []
-
-    for i in range(len(file_data)):
-        n_num = file_data[i][1]
-        new_data.append(float(n_num))
-
-
-    n = len(new_data)
-    total = 0
-
-    for x in new_data:
-        total += x
-
-    mean = total/n
-    print(mean)
-
-def median():
-    import csv
-
-    with open('SOCR-HeightWeight.csv') as f:
-        reader = csv.reader(f)
-        file_data = list(reader)
-
-    file_data.pop(0)
-
-    new_data = []
-
-    for i in range(len(file_data)):
-        n_num = file_data[i][1]
-        new_data.append(float(n_num))
-
-    n = len(new_data)
-    new_data.sort()
-
-    if n%2 == 0:
-        median1 = float(new_data[n//2])
-        median2 = float(new_data[n//2-1])
-        median = (median1 + median2)/2
+def median(total_entries, sorted_data):
+    if total_entries % 2 == 0:
+        median1 = float(sorted_data[total_entries//2])
+        median2 = float(sorted_data[total_entries//2 - 1])
+        median = (median1 + median2) / 2
     else:
-        median = new_data[n//2]
+        median = float(sorted_data[total_entries//2])
+    print(f"Median is -> {median:2f}")
 
-    print(median)
-
-def mode():
-    import csv
-    from collections import Counter
-
-    with open('SOCR-HeightWeight.csv') as f:
-        reader = csv.reader(f)
-        file_data = list(reader)
-
-    file_data.pop(0)
-
-    new_data = []
-
-    for i in range(len(file_data)):
-        n_num = file_data[i][1]
-        new_data.append(n_num)
-
-    data = Counter(new_data)
-    data_for_range = {
-        '50-60':0,
-        '60-70':0,
-        '70-80':0
-    }
-
-    for height, occurence in data.items():
-        if 50 < float(height) < 60:
-            data_for_range['50-60'] += occurence
-        elif 60 < float(height) < 70:
-            data_for_range['60-70'] += occurence
-        elif 70 < float(height) < 80:
-            data_for_range['70-80'] += occurence
-
-    mode_range, mode_occurence = 0,0
-
-    for range, occurence in data_for_range.items():
+def mode(sorted_data):
+    data = Counter(sorted_data)
+    mode_data_for_range = {
+                            "75-85": 0,
+                            "85-95": 0,
+                            "95-105": 0,
+                            "105-115": 0,
+                            "115-125": 0,
+                            "125-135": 0,
+                            "135-145": 0,
+                            "145-155": 0,
+                            "155-165": 0,
+                            "165-175": 0
+                        }
+    for weight, occurence in data.items():
+        if 75 < weight < 85:
+            mode_data_for_range["75-85"] += occurence
+        elif 85 < weight < 95:
+            mode_data_for_range["85-95"] += occurence
+        elif 95 < weight < 105:
+            mode_data_for_range["95-105"] += occurence
+        elif 105 < weight < 115:
+            mode_data_for_range["105-115"] += occurence
+        elif 115 < weight < 125:
+            mode_data_for_range["115-125"] += occurence
+        elif 125 < weight < 135:
+            mode_data_for_range["125-135"] += occurence
+        elif 135 < weight < 145:
+            mode_data_for_range["135-145"] += occurence
+        elif 145 < weight < 155:
+            mode_data_for_range["145-155"] += occurence
+        elif 155 < weight < 165:
+            mode_data_for_range["155-165"] += occurence
+        elif 165 < weight < 175:
+            mode_data_for_range["165-175"] += occurence
+    mode_range, mode_occurence = 0, 0
+    for range, occurence in mode_data_for_range.items():
         if occurence > mode_occurence:
-            mode_range, mode_occurence = [int(range.split('-')[0]), int(range.split('-')[1])], occurence
-            mode = float((mode_range[0] + mode_range[1])/2)
-
+            mode_range, mode_occurence = [int(range.split("-")[0]), int(range.split("-")[1])], occurence
+    mode = float((mode_range[0] + mode_range[1]) / 2)
     print(f"Mode is -> {mode:2f}")
 
-mean()
-median()
-mode()
+with open('SOCR-HeightWeight.csv', newline='') as f:
+    reader = csv.reader(f)
+    file_data = list(reader)
+
+file_data.pop(0)
+
+total_weight = 0
+total_entries = len(file_data)
+sorted_data = []
+
+for person_data in file_data:
+    total_weight += float(person_data[2])
+    sorted_data.append(float(person_data[2]))
+
+sorted_data.sort()
+
+mean(total_weight, total_entries)
+median(total_entries, sorted_data)
+mode(sorted_data)
